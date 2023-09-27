3b369935eb50ad8c29763861b8017424e8ae1475
lleoeric <62811623+lleoeric@users.noreply.github.com>
2023-09-27 18:16:52 +0800

    初始化项目

---
+++ src/main/java/cn/leoeric/jpa_learning/JpaLearningApplication.java
@@ -0,0 +1,14 @@
+package cn.leoeric.jpa_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaLearningApplication.class, args);
+
+    }
+
+}
